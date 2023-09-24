a64fccded237e096604c4a9e41d9cf400d60b02a
422UR4H <tiagosindra@id.uff.br>
2023-09-24 02:51:27 -0300

    :sparkles: feat/get-tickets

--- src/repositories/tickets-repository.ts
+++ src/repositories/tickets-repository.ts
@@ -1,20 +1,23 @@
 import { prisma } from '@/config';
+import { enrollmentRepository } from './enrollments-repository';
 
 function findTypes() {
     return prisma.ticketType.findMany();
 }
 
 function findTickets(id: number) {
-    return prisma.user.findFirst({
-        where: { id },
-        select: {
-            Enrollment: {
-                select: { Ticket: true }
-            }
-        }
+    return prisma.enrollment.findFirst({
+        where: { userId: id },
+        select: { Ticket: true }
+    });
+}
+
+function createTicket(ticketTypeId: number, enrollmentId: number) {
+    return prisma.ticket.create({
+        data: { ticketTypeId, enrollmentId, status: "RESERVED" }
     });
 }
 
 export const ticketRepository = {
-    findTypes, findTickets
+    findTypes, findTickets, createTicket
 };
