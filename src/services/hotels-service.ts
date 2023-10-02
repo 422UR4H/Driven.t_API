ee4949849b73bd3f48608ad5d27646fe3847a12b
422UR4H <tiagosindra@id.uff.br>
2023-10-02 16:03:00 -0300

    :bug: fix/hotels-service

--- src/services/hotels-service.ts
+++ src/services/hotels-service.ts
@@ -1,4 +1,4 @@
-import { notFoundError } from '@/errors';
+import { notFoundError, unauthorizedError } from '@/errors';
 import { paymentRequiredError } from '@/errors/payment-required-error';
 import { enrollmentRepository, ticketsRepository } from '@/repositories';
 import { hotelsRepository } from '@/repositories/hotels-repository';
@@ -8,8 +8,8 @@ async function getAllHotels(userId: number) {
     const enrollment = await enrollmentRepository.findEnrollmentId(userId);
     if (enrollment == null) throw notFoundError();
 
-    const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
-    if (ticket == null) throw notFoundError();
+    const ticket = await ticketsRepository.findTicketByEnrollmentId(userId);
+    if (ticket?.Ticket == null) throw notFoundError();
 
     const { status, TicketType } = ticket.Ticket;
     if (status !== TicketStatus.PAID) throw paymentRequiredError();
@@ -23,8 +23,8 @@ async function getHotelById(hotelId: number, userId: number) {
     const enrollment = await enrollmentRepository.findEnrollmentId(userId);
     if (enrollment == null) throw notFoundError();
 
-    const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
-    if (ticket == null) throw notFoundError();
+    const ticket = await ticketsRepository.findTicketByEnrollmentId(userId);
+    if (ticket?.Ticket == null) throw notFoundError();
 
     const { status, TicketType } = ticket.Ticket;
     if (status !== TicketStatus.PAID) throw paymentRequiredError();
