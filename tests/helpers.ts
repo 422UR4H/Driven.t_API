47c9fc5c851a42afda64df5b7e7b2694774a6dd7
422UR4H <tiagosindra@id.uff.br>
2023-10-02 17:37:32 -0300

    :fix: bug/Driven

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -10,7 +10,7 @@ export async function cleanDb() {
   await prisma.ticket.deleteMany({});
   await prisma.ticketType.deleteMany({});
   await prisma.enrollment.deleteMany({});
-  // await prisma.event.deleteMany({});
+  await prisma.event.deleteMany({});
   await prisma.session.deleteMany({});
   await prisma.booking.deleteMany({});
   await prisma.user.deleteMany({});
--- tests/integration/hotels.test.ts
+++ tests/integration/hotels.test.ts
@@ -12,6 +12,7 @@ import {
 import { TicketStatus } from '@prisma/client';
 import { prisma } from '@/config';
 import { createHotelsWithRooms } from '../factories/hotels-factory';
+import dayjs from 'dayjs';
 
 
 const server = supertest(app);
@@ -22,6 +23,19 @@ beforeAll(async () => {
 
 beforeEach(async () => {
     await cleanDb();
+
+    let event = await prisma.event.findFirst();
+    if (!event) {
+        event = await prisma.event.create({
+            data: {
+                title: "Driven.t",
+                logoImageUrl: "https://files.driven.com.br/images/logo-rounded.png",
+                backgroundImageUrl: "linear-gradient(to right, #FA4098, #FFD77F)",
+                startsAt: dayjs().toDate(),
+                endsAt: dayjs().add(21, "days").toDate(),
+            },
+        });
+    }
 });
 
 describe('GET /hotels/:hotelId', () => {
